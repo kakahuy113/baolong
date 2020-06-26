70626b3eb3526a05141b94ed79052dfa6a386dc9
hoangviethung <hoangviet.hung.009@gmail.com>
2020-06-26 09:47:03 +0700

    HEADER SECTION INDEX 2

--- src/js/main.js
+++ src/js/main.js
@@ -4,10 +4,62 @@ import {
 } from './util/utilities';
 import Cookie from './lib/Cookie';
 
+// MAIN BANNER WEBSITE
+const mainBanner = () => {
+	let mainBanner = new Swiper('.MainSlider__Banners .swiper-container', {
+		effect: 'fade',
+		fadeEffect: {
+			crossFade: true,
+		},
+		centeredSlides: true,
+		speed: 1000,
+		loop: true,
+		autoplay: {
+			delay: 5000,
+			disableOnInteraction: false,
+		},
+		pagination: {
+			el: '.MainSlider__Banners .swiper-pagination',
+			type: 'bullets',
+			clickable: true,
+		},
+	})
+}
+
+// INITALIZE SUBMENU
+const initializeSubMenu = () => {
+	const itemsBottomHeader = document.querySelectorAll(
+		'.navBar__itemWrapper .navBar--item'
+	);
+	itemsBottomHeader.forEach((item) => {
+		const sub = item.querySelector('.navBar--subMenu');
+		if (sub) {
+			item.classList.add('hasSub');
+		}
+	});
+};
+
+const showMenuMobile = () => {
+	const btn = document.querySelector('.navBarHamburger__mainWrapper');
+	const menu = document.querySelector('.navBar__itemWrapper');
+	if (btn) {
+		btn.addEventListener('click', () => {
+			btn.classList.toggle('active');
+			menu.classList.toggle('active');
+		})
+	}
+}
+
 document.addEventListener('DOMContentLoaded', () => {
 	Cookie();
 	getSVGs();
 	Loading();
+	// MAIN BANNER WEBSITE
+	mainBanner();
+	// INITALIZE SUBMENU
+	initializeSubMenu();
+	// SHOW MENU MOBILE
+	showMenuMobile();
 });
 
 document.addEventListener('DOMContentLoaded', () => {});
