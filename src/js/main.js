e5c6cb07b021d5b8905832f1f9e5bc681ac0766f
Lê Huỳnh Song Toàn <lehuynhsongtoan@gmail.com>
2020-06-29 17:08:59 +0700

    update research

--- src/js/main.js
+++ src/js/main.js
@@ -1,65 +1,80 @@
 import {
-	getSVGs,
-	Loading
+    getSVGs,
+    Loading
 } from './util/utilities';
 import Cookie from './lib/Cookie';
 
 // MAIN BANNER WEBSITE
 const mainBanner = () => {
-	let mainBanner = new Swiper('.MainSlider__Banners .swiper-container', {
-		effect: 'fade',
-		fadeEffect: {
-			crossFade: true,
-		},
-		centeredSlides: true,
-		speed: 1000,
-		loop: true,
-		autoplay: {
-			delay: 5000,
-			disableOnInteraction: false,
-		},
-		pagination: {
-			el: '.MainSlider__Banners .swiper-pagination',
-			type: 'bullets',
-			clickable: true,
-		},
-	})
+    let mainBanner = new Swiper('.MainSlider__Banners .swiper-container', {
+        effect: 'fade',
+        fadeEffect: {
+            crossFade: true,
+        },
+        centeredSlides: true,
+        speed: 1000,
+        loop: true,
+        autoplay: {
+            delay: 5000,
+            disableOnInteraction: false,
+        },
+        pagination: {
+            el: '.MainSlider__Banners .swiper-pagination',
+            type: 'bullets',
+            clickable: true,
+        },
+    })
 }
 
 // INITALIZE SUBMENU
 const initializeSubMenu = () => {
-	const itemsBottomHeader = document.querySelectorAll(
-		'.navBar__itemWrapper .navBar--item'
-	);
-	itemsBottomHeader.forEach((item) => {
-		const sub = item.querySelector('.navBar--subMenu');
-		if (sub) {
-			item.classList.add('hasSub');
-		}
-	});
+    const itemsBottomHeader = document.querySelectorAll(
+        '.navBar__itemWrapper .navBar--item'
+    );
+    itemsBottomHeader.forEach((item) => {
+        const sub = item.querySelector('.navBar--subMenu');
+        if (sub) {
+            item.classList.add('hasSub');
+        }
+    });
 };
 
 const showMenuMobile = () => {
-	const btn = document.querySelector('.navBarHamburger__mainWrapper');
-	const menu = document.querySelector('.navBar__itemWrapper');
-	if (btn) {
-		btn.addEventListener('click', () => {
-			btn.classList.toggle('active');
-			menu.classList.toggle('active');
-		})
-	}
+    const btn = document.querySelector('.navBarHamburger__mainWrapper');
+    const menu = document.querySelector('.navBar__itemWrapper');
+    if (btn) {
+        btn.addEventListener('click', () => {
+            btn.classList.toggle('active');
+            menu.classList.toggle('active');
+        })
+    }
 }
 
+const searchOnMenu = () => {
+    // $(".search--input").hide();
+    $(".search .btn").click(function(e) {
+        e.preventDefault();
+        $(".search--input").toggleClass('active--search');
+        if ($(".search--input").hasClass("active--search")) {
+            $(".search .btn img").attr("src", "./assets/icons/close.svg");
+            $(".search .btn img").attr("data-src", "./assets/icons/close.svg");
+        } else {
+            $(".search .btn img").attr("src", "./assets/icons/search.svg");
+            $(".search .btn img").attr("data-src", "./assets/icons/search.svg");
+        }
+    });
+}
 document.addEventListener('DOMContentLoaded', () => {
-	Cookie();
-	getSVGs();
-	Loading();
-	// MAIN BANNER WEBSITE
-	mainBanner();
-	// INITALIZE SUBMENU
-	initializeSubMenu();
-	// SHOW MENU MOBILE
-	showMenuMobile();
+    Cookie();
+    getSVGs();
+    Loading();
+    // MAIN BANNER WEBSITE
+    mainBanner();
+    // INITALIZE SUBMENU
+    initializeSubMenu();
+    // SHOW MENU MOBILE
+    showMenuMobile();
+    searchOnMenu();
 });
 
 document.addEventListener('DOMContentLoaded', () => {});
