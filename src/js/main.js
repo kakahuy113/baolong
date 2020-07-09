72b05c55fdb5d54f378179b4d7af93f83d8d5605
Lê Huỳnh Song Toàn <lehuynhsongtoan@gmail.com>
2020-07-09 12:51:15 +0700

    update table

--- src/js/main.js
+++ src/js/main.js
@@ -51,7 +51,7 @@ const sliderMembers = () => {
 
 // ACTIVE HEADER WHEN SCROLL
 const activeHeader = () => {
-    $(window).scroll(function () {
+    $(window).scroll(function() {
         if ($(this).scrollTop() > 150) {
             $('header').addClass('scrolled');
         } else {
@@ -67,7 +67,7 @@ const initializeWow = () => {
         offset: 0, // distance to the element when triggering the animation (default is 0)
         mobile: true, // trigger animations on mobile devices (default is true)
         live: true, // act on asynchronously loaded content (default is true)
-        callback: function (box) {
+        callback: function(box) {
             // the callback is fired every time an animation is started
             // the argument that is passed in is the DOM node being animated
         },
@@ -124,25 +124,25 @@ const searchOnMenu = () => {
 
 // INIT DATE PICKER
 const initDatePicker = () => {
-    $(".date-picker").each(function () {
+    $(".date-picker").each(function() {
         $(this).flatpickr({
-            dateFormat: "Y-m-d H:i",
-            time_24hr: true,
-            disableMobile: "true"
-        })
-        // IF...ELSE
-        // if ($(this).val().length > 0) {
-        // 	$(this).flatpickr({
-        // 		dateFormat: "Y-m-d H:i",
-        // 		time_24hr: true,
-        // 	})
-        // } else {
-        // 	$(this).flatpickr({
-        // 		dateFormat: "Y-m-d H:i",
-        // 		time_24hr: true,
-        // 		defaultDate: new Date()
-        // 	})
-        // }
+                dateFormat: "Y-m-d H:i",
+                time_24hr: true,
+                disableMobile: "true"
+            })
+            // IF...ELSE
+            // if ($(this).val().length > 0) {
+            // 	$(this).flatpickr({
+            // 		dateFormat: "Y-m-d H:i",
+            // 		time_24hr: true,
+            // 	})
+            // } else {
+            // 	$(this).flatpickr({
+            // 		dateFormat: "Y-m-d H:i",
+            // 		time_24hr: true,
+            // 		defaultDate: new Date()
+            // 	})
+            // }
     })
 }
 
@@ -174,20 +174,20 @@ const readPdfFancyBox = () => {
 
 // check banner => add class
 const checkBanner = () => {
-	let banner = document.querySelector("section.TitlePage__Banners");
-	let header = document.querySelectorAll('header');
-	let heightHeader = header.offsetHeight;
-	let main = document.querySelector('main');
-	let mainBanner = document.querySelector(".MainSlider__Banners");
-	if ((!banner)) {
-		if (!mainBanner) {
-			if (main) {
-				main.style.paddingTop = heightHeader + 'px';
-			}
-		}
-	} else {
-		document.querySelector("div.header__top").classList.add("template--2");
-	}
+    let banner = document.querySelector("section.TitlePage__Banners");
+    let header = document.querySelectorAll('header');
+    let heightHeader = header.offsetHeight;
+    let main = document.querySelector('main');
+    let mainBanner = document.querySelector(".MainSlider__Banners");
+    if ((!banner)) {
+        if (!mainBanner) {
+            if (main) {
+                main.style.paddingTop = heightHeader + 'px';
+            }
+        }
+    } else {
+        document.querySelector("div.header__top").classList.add("template--2");
+    }
 }
 
 // ACTIVE LINK MENU
@@ -199,7 +199,7 @@ const activeLinkMenu = () => {
     } else {
         link = url[(url.length - 1)];
     }
-    $('nav.navBar__itemWrapper .navBar--item a').each(function () {
+    $('nav.navBar__itemWrapper .navBar--item a').each(function() {
         var getHref = $(this).attr('href');
         var href = getHref.split('/').pop();
         if (href === link) {
@@ -207,7 +207,7 @@ const activeLinkMenu = () => {
             $(this).parent().addClass('active');
         }
     });
-    $('a.footer__site-maps--link').each(function () {
+    $('a.footer__site-maps--link').each(function() {
         var getHref = $(this).attr('href');
         var href = getHref.split('/').pop();
         if (href === link) {
@@ -219,26 +219,27 @@ const activeLinkMenu = () => {
 
 // CHANGE CONTENT TABLE MOBILE
 const changeContentMobile = () => {
-    const contentsMobile = document.querySelectorAll('[data-content--mobile]');
-    const contentsPc = document.querySelectorAll('[data-content--pc]');
-    const isChange = window.innerWidth < 1025;
-    if (isChange == true) {
-        contentsMobile.forEach((item) => {
-            const valueContent = item.getAttribute('data-content--mobile');
-            item.innerHTML = valueContent;
-        })
-    } else {
-        contentsPc.forEach((item) => {
-            const valueContent = item.getAttribute('data-content--pc');
-            item.innerHTML = valueContent;
-        })
+        const contentsMobile = document.querySelectorAll('[data-content--mobile]');
+        const contentsPc = document.querySelectorAll('[data-content--pc]');
+        const isChange = window.innerWidth < 1025;
+        if (isChange == true) {
+            contentsMobile.forEach((item) => {
+                const valueContent = item.getAttribute('data-content--mobile');
+                item.innerHTML = valueContent;
+            })
+        } else {
+            contentsPc.forEach((item) => {
+                const valueContent = item.getAttribute('data-content--pc');
+                item.innerHTML = valueContent;
+            })
+        }
     }
-}
-//slide toggle category
+    //slide toggle category
 const showMoreContentPrograms = () => {
     const rowContent = document.querySelectorAll(".programsTableContent--item");
     const noBorder = document.querySelectorAll(".no-border");
-    $(".programsTableContent--item").click(function (e) {
+    $(noBorder).addClass("pe-n");
+    $(".programsTableContent--item").click(function(e) {
         e.preventDefault();
         if ($(this).hasClass("active")) {
             rowContent.forEach((item) => {
@@ -255,16 +256,18 @@ const showMoreContentPrograms = () => {
                 item.classList.add("opacity--2");
             })
             noBorder.forEach((item) => {
-                item.classList.remove("opacity--2");
-            })
-            //hidden all item list
+                    item.classList.remove("opacity--2");
+                })
+                //hidden all item list
             $(this).removeClass("opacity--2");
             // add class active => item => click 
             $(this).addClass("active");
             // show content category
             $(this).find(".show--more").css("display", "flex");
             // find chil col-9 add hidden
-            $(this).children(".row").eq(0).children(".col-9").hide();
+            if ($(this).hasClass("hasSubContent__programs")) {
+                $(this).children(".row").eq(0).children(".col-9").hide();
+            }
             // add border category
             // $(this).children(".row").eq(1).
         }
@@ -288,142 +291,142 @@ const activeHomeFormRegister = () => {
 }
 
 const addMember = () => {
-	var btnAddMember = false;
-	// Complete AddMember
-	$('.btn--submitForm').click(() => {
-		const inputs = document.querySelectorAll('.homeFormRegister__subForm input');
-			if (inputs[0].value != '' && inputs[1].value != '' && inputs[0].value != 'null' && inputs[1].value != 'null') {
-				btnAddMember = true;
-				document.querySelectorAll(".memberRegister--name").forEach(item => {
-					if(item.innerHTML == document.querySelector(".subForm--memberRegisterName").innerHTML) {
-						item.setAttribute("data-name" , `${$('.homeFormRegister__subForm .input-for-name').val()}`)
-						item.setAttribute("data-birthday" , `${$('.homeFormRegister__subForm .date-picker').val()}`)
-					} else return;
-				})
-				$('.homeFormRegister__subForm').css('display', 'none');
-				inputs.forEach(item => {
-					item.value = ''
-				})
-			} else return;
-		document.querySelectorAll('.memberRegister--name').forEach((item) => {
-			item.parentNode.classList.remove('active');
-		});
-	});
-	// Add Member
-	const memberItem = document.querySelector('.memberRegister--item');
-	const contentMember = document.querySelector('.memberRegister--name');
-	const btnRegisterMember = document.querySelector('.homeFormRegister__btnAddMore .btn-icon');
-	if (btnRegisterMember) {
-		btnRegisterMember.addEventListener('click', () => {
-			if (btnAddMember == true) {
-				const HowmanyMember = document.querySelectorAll(
-					'.memberRegister--item'
-				).length;
-				const tempItem = memberItem.outerHTML.replace(
-					`${contentMember.innerHTML}`,
-					`THÀNH VIÊN ${HowmanyMember + 1}`
-				);
-				const anothertemp = tempItem.replace("data-name" , "")
-				const aanothertemp = anothertemp.replace("data-birthday" , "")
-				$('.memberRegister--list').append(`${aanothertemp}`);
-			}
-			btnAddMember = false;
-			deleteMember();
-			EditMember();
-			const contenMemberShow = document.querySelectorAll('.memberRegister--name')
-			const tempnamelength = contenMemberShow.length
-			const tempname = contenMemberShow[tempnamelength - 1].innerHTML
-			contenMemberShow[tempnamelength - 1].parentNode.classList.add('active');
-			document.querySelector(".subForm--memberRegisterName").innerHTML = tempname;
-			document.querySelector('.homeFormRegister__subForm').style.display = 'block'
-		});
-	}
+    var btnAddMember = false;
+    // Complete AddMember
+    $('.btn--submitForm').click(() => {
+        const inputs = document.querySelectorAll('.homeFormRegister__subForm input');
+        if (inputs[0].value != '' && inputs[1].value != '' && inputs[0].value != 'null' && inputs[1].value != 'null') {
+            btnAddMember = true;
+            document.querySelectorAll(".memberRegister--name").forEach(item => {
+                if (item.innerHTML == document.querySelector(".subForm--memberRegisterName").innerHTML) {
+                    item.setAttribute("data-name", `${$('.homeFormRegister__subForm .input-for-name').val()}`)
+                    item.setAttribute("data-birthday", `${$('.homeFormRegister__subForm .date-picker').val()}`)
+                } else return;
+            })
+            $('.homeFormRegister__subForm').css('display', 'none');
+            inputs.forEach(item => {
+                item.value = ''
+            })
+        } else return;
+        document.querySelectorAll('.memberRegister--name').forEach((item) => {
+            item.parentNode.classList.remove('active');
+        });
+    });
+    // Add Member
+    const memberItem = document.querySelector('.memberRegister--item');
+    const contentMember = document.querySelector('.memberRegister--name');
+    const btnRegisterMember = document.querySelector('.homeFormRegister__btnAddMore .btn-icon');
+    if (btnRegisterMember) {
+        btnRegisterMember.addEventListener('click', () => {
+            if (btnAddMember == true) {
+                const HowmanyMember = document.querySelectorAll(
+                    '.memberRegister--item'
+                ).length;
+                const tempItem = memberItem.outerHTML.replace(
+                    `${contentMember.innerHTML}`,
+                    `THÀNH VIÊN ${HowmanyMember + 1}`
+                );
+                const anothertemp = tempItem.replace("data-name", "")
+                const aanothertemp = anothertemp.replace("data-birthday", "")
+                $('.memberRegister--list').append(`${aanothertemp}`);
+            }
+            btnAddMember = false;
+            deleteMember();
+            EditMember();
+            const contenMemberShow = document.querySelectorAll('.memberRegister--name')
+            const tempnamelength = contenMemberShow.length
+            const tempname = contenMemberShow[tempnamelength - 1].innerHTML
+            contenMemberShow[tempnamelength - 1].parentNode.classList.add('active');
+            document.querySelector(".subForm--memberRegisterName").innerHTML = tempname;
+            document.querySelector('.homeFormRegister__subForm').style.display = 'block'
+        });
+    }
 };
 
 //EditMember
 const EditMember = () => {
-	document
-		.querySelectorAll('.memberRegister--name')
-		.forEach((item, index) => {
-			item.parentNode.classList.remove('active');
-			item.addEventListener('click', () => {
-				document.querySelectorAll('.memberRegister--name')
-					.forEach((item) => {
-						item.parentNode.classList.remove('active');
-					});
-				item.parentNode.classList.add('active');
-				document.querySelector('.homeFormRegister__subForm')
-				let tempname= item.getAttribute("data-name")
-				let tempbirthDay = item.getAttribute("data-birthday")
-				$('.input-for-name').val(`${tempname}`);
-				$('.homeFormRegister__subForm .date-picker').val(`${tempbirthDay}`);
-				document.querySelector('.subForm--memberRegisterName').innerHTML = item.innerHTML;
-				document.querySelector('.homeFormRegister__subForm').style.display = 'block';
-			});
-		});
+    document
+        .querySelectorAll('.memberRegister--name')
+        .forEach((item, index) => {
+            item.parentNode.classList.remove('active');
+            item.addEventListener('click', () => {
+                document.querySelectorAll('.memberRegister--name')
+                    .forEach((item) => {
+                        item.parentNode.classList.remove('active');
+                    });
+                item.parentNode.classList.add('active');
+                document.querySelector('.homeFormRegister__subForm')
+                let tempname = item.getAttribute("data-name")
+                let tempbirthDay = item.getAttribute("data-birthday")
+                $('.input-for-name').val(`${tempname}`);
+                $('.homeFormRegister__subForm .date-picker').val(`${tempbirthDay}`);
+                document.querySelector('.subForm--memberRegisterName').innerHTML = item.innerHTML;
+                document.querySelector('.homeFormRegister__subForm').style.display = 'block';
+            });
+        });
 };
 
 const deleteMember = () => {
 
 
-	$(".subForm--listBtn .btn-icon").
-	click(() => {
-		document.querySelectorAll(".memberRegister--item").forEach(item => {
-			if (item.children[0].innerHTML == document.querySelector(".subForm--memberRegisterName").innerHTML) {
-				document.querySelector(".subForm--memberRegisterName").innerHTML = ""
-				item.remove();
-				document.querySelector('.homeFormRegister__subForm').style.display = 'none';
-			}
-		})
-		document.querySelectorAll(".memberRegister--name").forEach((item , index) => {
-			item.innerHTML = `THÀNH VIÊN ${index + 1}`
-		})
+    $(".subForm--listBtn .btn-icon").
+    click(() => {
+        document.querySelectorAll(".memberRegister--item").forEach(item => {
+            if (item.children[0].innerHTML == document.querySelector(".subForm--memberRegisterName").innerHTML) {
+                document.querySelector(".subForm--memberRegisterName").innerHTML = ""
+                item.remove();
+                document.querySelector('.homeFormRegister__subForm').style.display = 'none';
+            }
+        })
+        document.querySelectorAll(".memberRegister--name").forEach((item, index) => {
+            item.innerHTML = `THÀNH VIÊN ${index + 1}`
+        })
 
-	})
+    })
 }
 
 document.addEventListener('DOMContentLoaded', () => {
-	Cookie();
-	getSVGs();
-	Loading();
-	// INITALIZE WOW
-	initializeWow();
-	// INIT DATE PICKER
-	initDatePicker();
-	// MAIN BANNER WEBSITE
-	mainBanner();
-	// MEMBERS SLIDER
-	sliderMembers();
-	// INITALIZE SUBMENU
-	initializeSubMenu();
-	// SHOW MENU MOBILE
-	showMenuMobile();
-	//ACTIVE LINK MENU
-	activeLinkMenu();
-	// ACTIVE HEADER WHEN SCROLL
-	activeHeader();
-	// SHOW SEARCH
-	searchOnMenu();
-	//POPUP map contact
-	getMapContact();
-	// popup read pdf 
-	readPdfFancyBox();
-	// check banner
-	checkBanner();
-	// CHANGE CONTENT TABLE MOBILE
-	changeContentMobile();
-	// SHOW MORE CONTENT PROGRAMS
-	showMoreContentPrograms();
-	// ACTIVE HOME REGISTER
-	activeHomeFormRegister();
-	//Add Member
-	addMember();
-	
-	// TAB
-	const About = new Tab(".About .tab-container");
-	const Define = new Tab('.Define .tab-container');
-	const StepBuyOnline__1 = new Tab('.StepBuyOnline__1 .tab-container');
-	const PopUp__UpdateForms = new Tab('.PopUp__UpdateForms .tab-container');
+    Cookie();
+    getSVGs();
+    Loading();
+    // INITALIZE WOW
+    initializeWow();
+    // INIT DATE PICKER
+    initDatePicker();
+    // MAIN BANNER WEBSITE
+    mainBanner();
+    // MEMBERS SLIDER
+    sliderMembers();
+    // INITALIZE SUBMENU
+    initializeSubMenu();
+    // SHOW MENU MOBILE
+    showMenuMobile();
+    //ACTIVE LINK MENU
+    activeLinkMenu();
+    // ACTIVE HEADER WHEN SCROLL
+    activeHeader();
+    // SHOW SEARCH
+    searchOnMenu();
+    //POPUP map contact
+    getMapContact();
+    // popup read pdf 
+    readPdfFancyBox();
+    // check banner
+    checkBanner();
+    // CHANGE CONTENT TABLE MOBILE
+    changeContentMobile();
+    // SHOW MORE CONTENT PROGRAMS
+    showMoreContentPrograms();
+    // ACTIVE HOME REGISTER
+    activeHomeFormRegister();
+    //Add Member
+    addMember();
+
+    // TAB
+    const About = new Tab(".About .tab-container");
+    const Define = new Tab('.Define .tab-container');
+    const StepBuyOnline__1 = new Tab('.StepBuyOnline__1 .tab-container');
+    const PopUp__UpdateForms = new Tab('.PopUp__UpdateForms .tab-container');
 });
 
 document.addEventListener('scroll', () => {});
